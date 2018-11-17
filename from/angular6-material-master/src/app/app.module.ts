f41078640d55146088953538e39d98efea360efa
Your Name <you@example.com>
2018-11-17 16:58:49 +0530

    from

---
+++ from/angular6-material-master/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+  { path: 'first-page', component: FirstPageComponent},
+  { path: 'second-page', component: SecondPageComponent},
+  { path: 'third-page', component: ThirdPageComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyNavComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
